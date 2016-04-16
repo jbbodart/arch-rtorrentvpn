b9b49760278b62caf411817cbd33d41d3c6e1af3
jbbodart <jbbodart@yahoo.com>
2016-04-16 16:29:08 +0200

    Multiple fixes

--- apps/root/start.sh
+++ apps/root/start.sh
@@ -20,11 +20,6 @@ fi
 
 echo_log "[info] Creating config directories..."
  
-mkdir -p /config/openvpn
-mkdir -p /config/privoxy
-mkdir -p /config/nginx
-mkdir -p /config/rtorrent/session
-mkdir -p /config/rutorrent
 
 # set up data directory
 #########################
@@ -36,46 +31,10 @@ mkdir -p /data/watch
 chown -R nobody:users /data/incomplete /data/downloads /data/watch
 chmod -R 777 /data/incomplete /data/downloads /data/watch
 
-# system set up
-###############
-
-# set up DNS
-# add in OpenNIC public nameservers
-echo 'nameserver 192.71.249.83' > /etc/resolv.conf
-echo 'nameserver 87.98.175.85' >> /etc/resolv.conf
-echo 'nameserver 92.222.80.28' >> /etc/resolv.conf
-echo 'nameserver 5.135.183.146' >> /etc/resolv.conf
-
-# set up timezone
-rm -f /etc/localtime
-ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
-
-# set up sshd
-#############
-
-if [[ "${ENABLE_SSHD}" == "yes" ]]; then
-
-    echo_log "[info] Configuring OpenSSH sever..."
-    mkdir -p /root/.ssh
-    chmod 700 /root/.ssh
-
-    if [[ -f "/config/sshd/authorized_keys" ]]; then
-        cp -R /config/sshd/authorized_keys /root/.ssh/ && chmod 600 /root/.ssh/*
-    fi
-
-    LAN_IP=$(hostname -i)
-    #sed -i -e "s/#ListenAddress.*/ListenAddress $LAN_IP/g" /etc/ssh/sshd_config
-    sed -i -e "s/#Port 22/Port 2222/g" /etc/ssh/sshd_config
-    sed -i -e "s/#PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
-    sed -i -e "s/#PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-    sed -i -e "s/#PermitEmptyPasswords.*/PermitEmptyPasswords yes/g" /etc/ssh/sshd_config
-    sed -i -e "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
-
-    echo_log "[info] OpenSSH server configuration done"
-fi
-
 # set up openvpn
 ################
+
+mkdir -p /config/openvpn
 
 if [[ "${ENABLE_VPN}" == "yes" ]]; then
     echo_log "[info] Configuring OpenVPN client..."
@@ -117,11 +76,12 @@ fi
 
 if [[ "${ENABLE_PRIVOXY}" == "yes" ]]; then	
 	echo_log "[info] Configuring Privoxy"...
-
-	if [[ ! -f "/config/privoxy/config" ]]; then
-		cp -R /etc/privoxy/ /config/
+	
+	if [[ ! -d "/config/privoxy" ]]; then
+		mkdir /config/privoxy
+		cp -R /etc/privoxy/* /config/privoxy/
 	fi
-		
+	
 	LAN_IP=$(hostname -i)
 	sed -i -e "s/confdir \/etc\/privoxy/confdir \/config\/privoxy/g" /config/privoxy/config
 	sed -i -e "s/logdir \/var\/log\/privoxy/logdir \/config\/privoxy/g" /config/privoxy/config
@@ -132,27 +92,20 @@ fi
 
 # set up nginx
 ##############
-if [[ ! -f /config/nginx/nginx.conf ]]; then
+if [[ ! -d /config/nginx ]]; then
+    mkdir /config/nginx
+    if [[ ! -f /config/nginx/nginx.conf ]]; then
     cp /home/nobody/config/nginx/nginx.conf /config/nginx/
 fi
-
-# set up php-fpm
-################
-
-sed -i -e "s/;daemonize =.*/daemonize = no/g" /etc/php/php-fpm.conf
-sed -i -e "s/user = http/user = nobody/g" /etc/php/php-fpm.d/www.conf
-sed -i -e "s/group = http/group = users/g" //etc/php/php-fpm.d/www.conf
-sed -i -e "s/listen.owner = http/listen.owner = nobody/g" /etc/php/php-fpm.d/www.conf
-sed -i -e "s/listen.group = http/listen.group = users/g" /etc/php/php-fpm.d/www.conf
-
-#sed -i -e "s/open_basedir =.*/open_basedir = \/srv\/http\/:\/config\/rutorrent\//g" /etc/php/php.ini
-sed -i -e "s/open_basedir =.*/; open_basedir = /g" /etc/php/php.ini
 
 # set up rtorrent
 #################
 
-if [[ ! -f /config/rtorrent/rtorrent.rc ]]; then
-    cp /home/nobody/config/rtorrent/rtorrent.rc /config/rtorrent/rtorrent.rc
+if [[ ! -d /config/rtorrent ]]; then
+    mkdir -p /config/rtorrent/session
+    if [[ ! -f /config/rtorrent/rtorrent.rc ]]; then
+        cp /home/nobody/config/rtorrent/rtorrent.rc /config/rtorrent/rtorrent.rc
+    fi
 fi
 
 # set up rutorrent
@@ -160,21 +113,20 @@ fi
 
 mkdir -p /srv/http/rutorrent/tmp
 
-if [[ ! -f /config/rutorrent/conf/config.php ]]; then
-    rm -rf /config/rutorrent/conf
-    cp -a /srv/http/rutorrent/conf.dist /config/rutorrent/conf
+if [[ ! -d /config/rutorrent ]]; then
+    mkdir -p /config/rutorrent/conf
+    cp -a /srv/http/rutorrent/conf.dist/* /config/rutorrent/conf/
     cp -af /home/nobody/config/rutorrent/config.php /config/rutorrent/conf/
-    cp -af /home/nobody/config/rutorrent/autotools.dat /srv/http/rutorrent/share/settings/
-    rm -f /srv/http/rutorrent/conf
+    rm -rf /srv/http/rutorrent/conf
     ln -sf /config/rutorrent/conf /srv/http/rutorrent/conf
 fi
 
 # Select which plugins to enable
-enabled_plugins=("_getdir" "_noty" "_noty2" "_task" "autotools" "check_port" "chunks" "cookies" "cpuload" "data" "datadir" "diskspace" "erasedata" "extsearch" "geoip" "source" "tracklabels" "throttle" "trafic") 
+enabled_plugins=("_getdir" "_noty" "_noty2" "_task" "autotools" "check_port" "chunks" "cookies" "cpuload" "data" "datadir" "diskspace" "erasedata" "extsearch" "source" "tracklabels" "throttle" "trafic") 
 
 for i in $(ls -1 /srv/http/rutorrent/plugins) ; do 
     if [[ " ${enabled_plugins[@]} " =~ " ${i} " ]]; then
-	   echo -e "\n[$(basename ${i})]\nenabled=yes" >> /srv/http/rutorrent/conf/plugins.ini
+       echo -e "\n[$(basename ${i})]\nenabled=yes" >> /srv/http/rutorrent/conf/plugins.ini
     else
        echo -e "\n[$(basename ${i})]\nenabled=no" >> /srv/http/rutorrent/conf/plugins.ini
     fi
