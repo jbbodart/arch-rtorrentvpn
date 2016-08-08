ea6e314b2f8b74aa1a8721cc948b5bceb7752346
jbbodart <jbbodart@yahoo.com>
2016-08-08 22:29:18 +0200

    Update to last version

--- apps/root/update-resolv-conf.sh
+++ apps/root/update-resolv-conf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /home/nobody/functions.sh
 #
 # Parses DHCP options from openvpn to update resolv.conf
 # To use set as 'up' and 'down' script in your openvpn *.conf:
@@ -9,46 +8,60 @@ source /home/nobody/functions.sh
 # Used snippets of resolvconf script by Thomas Hood <jdthood@yahoo.co.uk>
 # and Chris Hanson
 # Licensed under the GNU GPL.  See /usr/share/common-licenses/GPL.
-#
+# 07/2013 colin@daedrum.net Fixed intet name
 # 05/2006 chlauber@bnc.ch
 #
 # Example envs set from openvpn:
 # foreign_option_1='dhcp-option DNS 193.43.27.132'
 # foreign_option_2='dhcp-option DNS 193.43.27.133'
 # foreign_option_3='dhcp-option DOMAIN be.bnc.ch'
+# foreign_option_4='dhcp-option DOMAIN-SEARCH bnc.local'
 
-[ -x /usr/sbin/resolvconf ] || exit 0
+## You might need to set the path manually here, i.e.
+RESOLVCONF=$(which resolvconf)
 
 case $script_type in
 
 up)
-        for optionname in ${!foreign_option_*} ; do
-                option="${!optionname}"
-                echo $option
-                part1=$(echo "$option" | cut -d " " -f 1)
-                if [ "$part1" == "dhcp-option" ] ; then
-                        part2=$(echo "$option" | cut -d " " -f 2)
-                        part3=$(echo "$option" | cut -d " " -f 3)
-                        if [ "$part2" == "DNS" ] ; then
-                                IF_DNS_NAMESERVERS="$IF_DNS_NAMESERVERS $part3"
-                        fi
-                        if [ "$part2" == "DOMAIN" ] ; then
-                                IF_DNS_SEARCH="$IF_DNS_SEARCH $part3"
-                        fi
-                fi
-        done
-        R=""
-        for SS in $IF_DNS_SEARCH ; do
-                R="${R}search $SS
+  for optionname in ${!foreign_option_*} ; do
+    option="${!optionname}"
+    echo $option
+    part1=$(echo "$option" | cut -d " " -f 1)
+    if [ "$part1" == "dhcp-option" ] ; then
+      part2=$(echo "$option" | cut -d " " -f 2)
+      part3=$(echo "$option" | cut -d " " -f 3)
+      if [ "$part2" == "DNS" ] ; then
+        IF_DNS_NAMESERVERS="$IF_DNS_NAMESERVERS $part3"
+      fi
+      if [[ "$part2" == "DOMAIN" || "$part2" == "DOMAIN-SEARCH" ]] ; then
+        IF_DNS_SEARCH="$IF_DNS_SEARCH $part3"
+      fi
+    fi
+  done
+  R=""
+  if [ "$IF_DNS_SEARCH" ]; then
+    R="search "
+    for DS in $IF_DNS_SEARCH ; do
+      R="${R} $DS"
+    done
+  R="${R}
 "
-        done
-        for NS in $IF_DNS_NAMESERVERS ; do
-                R="${R}nameserver $NS
+  fi
+
+  for NS in $IF_DNS_NAMESERVERS ; do
+    R="${R}nameserver $NS
 "
-        done
-        echo -n "$R" | /usr/sbin/resolvconf -a "${dev}.inet"
-        ;;
+  done
+  #echo -n "$R" | $RESOLVCONF -x -p -a "${dev}"
+  echo -n "$R" | $RESOLVCONF -x -a "${dev}.inet"
+  ;;
 down)
-        /usr/sbin/resolvconf -d "${dev}.inet"
-        ;;
+  $RESOLVCONF -d "${dev}.inet"
+  ;;
 esac
+
+# Workaround / jm@epiclabs.io 
+# force exit with no errors. Due to an apparent conflict with the Network Manager
+# $RESOLVCONF sometimes exits with error code 6 even though it has performed the
+# action correctly and OpenVPN shuts down.
+exit 0
